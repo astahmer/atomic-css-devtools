414d1e812ddf39f53744271a65f773b0ae3bc2a8
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-04-08 17:52:57 +0200

    fix: No handler registered in 'devtools' to accept messages with id 'resize'

--- entrypoints/devtools/main.ts
+++ entrypoints/devtools/main.ts
@@ -1,3 +1,10 @@
+import { onMessage } from "webext-bridge/devtools";
+
+onMessage("resize", () => {
+  // Dummy listener to prevent error:
+  // Error: [webext-bridge] No handler registered in 'devtools' to accept messages with id 'resize'
+});
+
 browser.devtools.panels.elements
   .createSidebarPane("Atomic CSS")
   .then((pane) => {
